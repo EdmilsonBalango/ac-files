c071ea27ae39150586353778879f18b9c2d9f414
Edmilson Balango <edmilsonbalango34@gmail.com>
2021-08-05 13:04:20 +0200

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {ThemeProvider} from 'styled-components';
+import { darkTheme, lightTheme } from './themes';
+import {GlobalStyles} from './global/styles'
+
+
 
 function App() {
+
+  const [theme, setTheme] = useState('light')
+
+  const themeTrigger = () =>{
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <div className="App">
+        <button onClick={()=> themeTrigger()}>Mudar</button>
+      </div>
+
+    
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/index.js
@@ -0,0 +1,9 @@
+export const lightTheme = {
+    body: '#fff',
+    fontColor: '#444'
+}
+
+export const darkTheme = {
+    body: '#222831',
+    fontColor: '#dddd'   
+}
